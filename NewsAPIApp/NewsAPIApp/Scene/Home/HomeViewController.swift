178e60ff9c9b6f108e771960464f249f4f53066f
slash585 <slsh585@gmail.com>
2024-08-04 02:42:26 +0300

    Init app

---
+++ NewsAPIApp/NewsAPIApp/Scene/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NewsAPIApp
+//
+//  Created by Mehmet Ali Özdemir on 4.08.2024.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
+
